74035e3085492e5602473e46b07e668f993d0b17
maksim <sysoyev.maxim@gmail.com>
2020-01-16 20:05:16 +0300

    ESP, DHT22 и Arduino

--- test/SoftwareSerial/ESP12_DHT22/ESP12_DHT22.ino
+++ test/SoftwareSerial/ESP12_DHT22/ESP12_DHT22.ino
@@ -1,13 +1,14 @@
 #include <DHT.h>
 #include <SoftwareSerial.h>
 
-#define DHTPIN -1
+#define DHTPIN 0
 DHT dht(DHTPIN, DHT22);
 
 SoftwareSerial mySerial(5, 4);
 
 void setup() {
   mySerial.begin(9600);
+  Serial.begin(9600);
   dht.begin();
 }
 
@@ -16,9 +17,16 @@ void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   if (isnan(h) || isnan(t)) { 
+    mySerial.println("Ошибка считывания");
     Serial.println("Ошибка считывания");
     return;
   }
+  mySerial.print("Влажность: ");
+  mySerial.print(h);
+  mySerial.print(" %\t");
+  mySerial.print("Температура: ");
+  mySerial.print(t);
+  mySerial.println(" *C "); 
   Serial.print("Влажность: ");
   Serial.print(h);
   Serial.print(" %\t");
